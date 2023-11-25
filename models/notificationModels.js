4db095a96dab4db61415c52f1b8f3e0f7e721aec
moussakaram <moussakaram2@gmail.com>
2023-11-25 22:06:54 +0200

    finish notification table

--- models/notificationModels.js
+++ models/notificationModels.js
@@ -11,22 +11,9 @@ const Notification = db.define('notification', {
    type: Sequelize.BOOLEAN,
    allowNull: false
  },
- received_id: {
-   type: Sequelize.INTEGER,
-   allowNull: false,
-   references: {
-     model: 'users', // assuming 'users' is the table name for User model
-     key: 'id'
-   }
- },
- transaction_id: {
-   type: Sequelize.INTEGER,
-   allowNull: false,
-   references: {
-     model: 'transactions', // assuming 'transactions' is the table name for Transaction model
-     key: 'id'
-   }
- }
+
+
+ 
 });
 
 export default Notification;
--- models/transactionsModels.js
+++ models/transactionsModels.js
@@ -1,53 +1,35 @@
-import { Sequelize, DataTypes } from 'sequelize';
-import User from './usersModels.js';
-import Promotion from './promotionModels.js';
-import db from '../config/dbconfig.js';
+import { DataTypes } from 'sequelize';
+import db from '../config/dbconfig.js'; 
+import User from './usersModels.js'; 
+import Promotion from './promotionModels.js'; 
 
+const Transaction = db.define('transaction', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
 
-  const Transaction = db.define('transaction', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    sender_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: User,
-        key: 'id'
-      }
-    },
-    recipient_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      references: {
-        model: User,
-        key: 'id'
-      }
-    },
-    amount: {
-      type: DataTypes.DECIMAL(10,2),
-      allowNull: false
-    },
-    date: {
-      type: DataTypes.DATE,
-      allowNull: false
-    },
-    promotion_id: {
-      type: DataTypes.INTEGER,
-      allowNull: true,
-      references: {
-        model: Promotion,
-        key: 'id'
-      }
-    },
-    amount_type: {
-      type: DataTypes.ENUM('credit', 'debit'),
-      allowNull: false
-    }
-  });
+  amount: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false
+  },
+  amount_type: {
+    type: DataTypes.ENUM('credit', 'debit'),
+    allowNull: false
+  },
+});
 
+// Define associations
+Transaction.belongsTo(User, { foreignKey: 'sender_id' });
+Transaction.belongsTo(Promotion, { foreignKey: 'promotion_id' });
+
+await db.sync({ force: false });
 
 
 export default Transaction;
