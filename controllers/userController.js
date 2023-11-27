3a0faaaa190fa7108b5977546a3f5ce398e34c42
moussakaram <moussakaram2@gmail.com>
2023-11-27 14:34:50 +0200

    testing user table

--- controllers/userController.js
+++ controllers/userController.js
@@ -35,11 +35,12 @@ export const getAllUsers = async (req, res) => {
 };
 
 // 3 - get single user
+// 3 - get single user
 export const getUserById = async (req, res) => {
-  const userId = req.params.id;
+  const { id } = req.params;
 
   try {
-    const user = await User.findByPk(userId);
+    const user = await User.findOne({ where: { id: id } });
     if (user) {
       res.json(user);
     } else {
@@ -51,13 +52,14 @@ export const getUserById = async (req, res) => {
   }
 };
 
+
   
 // 4 - delete user
 export const deleteUser = async (req, res) => {
   const { userId } = req.params;
 
   try {
-    const result = await User.destroy({ where: { userId: userId } });
+    const result = await User.destroy({ where: { id: userId } });
     if (result === 0) {
       return res.status(404).json({ message: "User not deleted" });
     }
@@ -65,22 +67,33 @@ export const deleteUser = async (req, res) => {
     res.status(200).json({ message: "User deleted successfully" });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Something went wrong!" });
+    res.status(500).json({ message: "Something went wrong!", error: error.message });
   }
-  
 };
 
-//5-update user
-export const updateUser = async (req,res,next) =>{
-    const { userId } = req.params;
-    try{
-        if(req.body){
-            const user=await User.update({...req.body},{where:{userId:userId}});
-            return res.status(200).json({message:`user updated successfully!`,user:user});
-        }
-        res.status(400).json({message:'something went wrong'})
 
-    }catch(err){console.error(err);
+//5-update user
+export const updateUser = async (req, res, next) => {
+  const { userId } = req.params;
+  try {
+    if (req.body) {
+      const [updatedRowsCount, updatedRows] = await User.update(
+        { ...req.body },
+        { where: { id: userId } }
+      );
+
+      if (updatedRowsCount > 0 && updatedRows && updatedRows.length > 0) {
+        return res
+          .status(200)
+          .json({ message: 'User updated successfully!', user: updatedRows[0] });
+      } else {
+        return res.status(404).json({ message: 'User not found' });
+      }
     }
 
-}
+    res.status(400).json({ message: 'Something went wrong' });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+};
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 
 router.post("/createUser", createUser);
 router.get("/getalluser", getAllUsers);
-router.get('/:userId', getUserById);
-router.delete('/:userId', deleteUser);
+router.get('/userId/:id', getUserById);
+router.delete('/deleteId/:userId', deleteUser);
 router.put('/:userId', updateUser);
 
 export default router;
